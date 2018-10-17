cbf7bdce1dd36161848bb7f35dc8565fef821520
Paweł Marcin Chojnacki <pawel.marcin.chojnacki@gmail.com>
2018-10-17 20:51:26 +0200

    Added routing to empty menu items.

--- PawelsHomePage/Controllers/HomeController.cs
+++ PawelsHomePage/Controllers/HomeController.cs
@@ -6,48 +6,28 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using PawelsHomePage.Models;
 
-namespace PawelsHomePage.Controllers
-{
-    public class HomeController : Controller
-    {
-        public IActionResult Index()
-        {
-            return View();
+namespace PawelsHomePage.Controllers {
+    public class HomeController : Controller {
+        public IActionResult Index () {
+            return View ();
+        }
+        
+        public IActionResult Portfolio () {
+            return View ();
+        }
+        public IActionResult Resume () {
+            return View ();
+        }
+        public IActionResult Social () {
+            return View ();
+        }
+        public IActionResult Blog () {
+            return Redirect ("https://pmchojnacki.azurewebsites.net/");
         }
 
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        public IActionResult Portfolio()
-        {
-            return View();
-        }
-
-        public IActionResult Blog()
-        {
-            return Redirect("https://pmchojnacki.azurewebsites.net/");
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        [ResponseCache (Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error () {
+            return View (new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
